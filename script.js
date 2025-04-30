23dc85f06158a1aec101eab55a98c15ba71b7ccc
Michael Pérez-Ortiz <perezortizm7@gmail.com>
2025-04-30 16:51:41 -0400

    tried and failed part 2 on prime numbers will come back to

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 //part1
-// for(let i = 1; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
 
-//     if(i % 3 === 0 && i % 5 ===0){
-//         console.log("fizzbuzz")
+    if(i % 3 === 0 && i % 5 ===0){
+        console.log("fizzbuzz")
        
       
-//     }
-//     else if(i % 3 ===0 && i % 5 !==0){
-//         console.log("fizz")
+    }
+    else if(i % 3 ===0 && i % 5 !==0){
+        console.log("fizz")
       
-//     }
-//     else if(i % 5 ===0 && i % 3 !==0){
-//         console.log("buzz")
+    }
+    else if(i % 5 ===0 && i % 3 !==0){
+        console.log("buzz")
 
-//     }else{
-//         console.log(i)
-//     }
+    }else{
+        console.log(i)
+    }
    
+    }
+
+//part2 not right
+
+// let n = 1;
+
+// while(n < 100){
+//     if(n % 2 ===0){
+//         n++
+//         continue
 //     }
-
-//part2
-
-let n = 1;
-
-while(n < 100){
-    if(n % 2 ===0){
-        continue
-    }
     
-    if(n % 3 ===0){
-        continue
-    }
+//     else if(n % 3 ===0){
+//         n++
+//         continue
+//     }
+//     n++
+//     console.log(n)
     
-    
-}
+// }
+
+
