00a5d6a51ea21f45d0dc4d4168670fdac4a1400b
Michael Pérez-Ortiz <perezortizm7@gmail.com>
2025-04-30 14:54:57 -0400

    completed part 1 started part 2

---
+++ script.js
@@ -0,0 +1,36 @@
+//part1
+// for(let i = 1; i <= 100; i++){
+
+//     if(i % 3 === 0 && i % 5 ===0){
+//         console.log("fizzbuzz")
+       
+      
+//     }
+//     else if(i % 3 ===0 && i % 5 !==0){
+//         console.log("fizz")
+      
+//     }
+//     else if(i % 5 ===0 && i % 3 !==0){
+//         console.log("buzz")
+
+//     }else{
+//         console.log(i)
+//     }
+   
+//     }
+
+//part2
+
+let n = 1;
+
+while(n < 100){
+    if(n % 2 ===0){
+        continue
+    }
+    
+    if(n % 3 ===0){
+        continue
+    }
+    
+    
+}
